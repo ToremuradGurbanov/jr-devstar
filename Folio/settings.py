3a815636545a951fae4bae6bd0a666cd43c02388
toremuradgurbanov <tmotuk@mail.ru>
2023-04-16 08:48:14 +0300

    setting has modified

--- Folio/settings.py
+++ Folio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEGUB', cast=bool)
+DEBUG = config("DEGUB", default = True, cast=bool)
 
 ALLOWED_HOSTS = []
 
